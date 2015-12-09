83d42e9d92707ff30f55a9dff9aad7843bf5ea7e
Christian M. Grube <cmg@triton.teiko.hq>
2015-12-09 09:35:35 +0100

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TwentySixteenChildTheme
 Eine Barebone um ein Childtheme für das WordPress Standardtheme Twentysixteen zu erstellen
+
+Siehe https://got-tty.org/archives/barebone-twentysixteen-childtheme.html
+
+TwentySixteen ist das neue Standardwordpresstheme für WordPress Version 4.4
+Weitere Informationen:
+https://make.wordpress.org/core/2015/08/25/introducing-twenty-sixteen/
+https://de.wordpress.org/themes/twentysixteen/
+
---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
+
+// EXAMPLEFUNCTION FOR THUMBNAILS IN OUR RSS FEED, cloud be delteted 
+function featuredtoRSS($content) {
+global $post;
+if ( has_post_thumbnail( $post->ID ) ){
+$content = '<div>' . get_the_post_thumbnail( $post->ID, 'medium', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
+}
+return $content;
+}
+ 
+add_filter('the_excerpt_rss', 'featuredtoRSS');
+add_filter('the_content_feed', 'featuredtoRSS');
+
